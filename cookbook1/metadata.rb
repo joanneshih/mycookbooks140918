55a58b37259ccea2101b0c349a9c7620579c9103
Joanne <joanneshih@joanneshih-NB2.msi.com.tw>
2014-09-17 18:42:09 +0800

    Signed-off-by: Joanne <joanneshih@joanneshih-NB2.msi.com.tw>

--- cookbook1/metadata.rb
+++ cookbook1/metadata.rb
@@ -1,8 +1,6 @@
 name             "cookbook1"
-maintainer       "MSI"
-maintainer_email "joanneshih@msi.com"
+maintainer       "MSI" 
 license          "All rights reserved"
 description      "Installs/Configures shared log" 
 version          "0.0.1"
-supports 'ubuntu'
-recipe "cookbook1::default", "Only one recipe"
+ 
