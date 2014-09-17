3368f4590b4cb55683c7bc298b96cba4ba5a9c26
Joanne <joanneshih@joanneshih-NB2.msi.com.tw>
2014-09-17 18:32:20 +0800

    Signed-off-by: Joanne <joanneshih@joanneshih-NB2.msi.com.tw>

---
+++ cookbook1/metadata.rb
@@ -0,0 +1,8 @@
+name             "cookbook1"
+maintainer       "MSI"
+maintainer_email "joanneshih@msi.com"
+license          "All rights reserved"
+description      "Installs/Configures shared log" 
+version          "0.0.1"
+supports 'ubuntu'
+recipe "cookbook1::default", "Only one recipe"
---
+++ cookbook1/recipes/default.rb
@@ -0,0 +1,5 @@
+node[:deploy].each do |application, deploy|
+  share_file= "#{deploy[:deploy_to]}/shared/log/*"
+  execute "chmod -R g+rw #{share_root}" do
+  end
+end
