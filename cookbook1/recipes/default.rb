112e88ca9ede7f807c8e02682a6511462628efa9
Joanne <joanneshih@joanneshih-NB2.msi.com.tw>
2014-09-18 11:57:39 +0800

    Signed-off-by: Joanne <joanneshih@joanneshih-NB2.msi.com.tw>

--- cookbook1/recipes/default.rb
+++ cookbook1/recipes/default.rb
@@ -1,11 +1,5 @@
 node[:deploy].each do |application, deploy|
-Dir[ "#{deploy[:deploy_to]}/shared/log/*" ].each do |path|
-  file path do
-    owner "www-data"
-    group "www-data"
-  end if File.file?(path)
-  directory path do
-    owner "www-data"
-    group "www-data"
-  end if File.directory?(path)
-end
+  share_file= "#{deploy[:deploy_to]}/shared/log/*"
+  execute "chmod -R g+rw #{share_root}" do
+  end
+end 
