02f5c0267a26cde8c8df4753f6324dab37584b1b
Joanne <joanneshih@joanneshih-NB2.msi.com.tw>
2014-09-18 18:24:41 +0800

    Signed-off-by: Joanne <joanneshih@joanneshih-NB2.msi.com.tw>

--- cookbook1/recipes/default.rb
+++ cookbook1/recipes/default.rb
@@ -1,6 +1,6 @@
 
 node[:deploy].each do |application, deploy|
-  execute "chmod 777 #{deploy[:deploy_to]}/shared/log/*" 
+  execute "chmod 777 #{deploy[:deploy_to]}/shared/log/*.log" 
     not_if "test -f #{deploy[:deploy_to]}/shared/log/"
   end
 end
